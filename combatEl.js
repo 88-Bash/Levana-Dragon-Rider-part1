bc77d818cbc53e67d7ae47eb8d2345a1b1e16a45
Sebastian Szajuk <szajuk.sebastian@gmail.com>
2023-11-01 20:37:40 +0100

    status bar div is set up for multiple character for both sides

--- combatEl.js
+++ combatEl.js
@@ -1,3 +1,4 @@
+// Hero status bar
 const healthHero = document.querySelector("#healthHero");
 const armorHero = document.querySelector("#armorHero");
 const ffHero = document.querySelector("#ffHero");
@@ -18,3 +19,27 @@ function heroStatusBar(heroHealth, heroArmor, heroFf) {
 }
 
 heroStatusBar(heroHealth, heroArmor, heroFf);
+
+// Enemy status bar
+
+const healthEnemy = document.querySelector("#healthEnemy");
+const armorEnemy = document.querySelector("#armorEnemy");
+const ffEnemy = document.querySelector("#ffEnemy");
+
+var enemyHealth = 100;
+var enemyArmor = 25;
+var enemyFf = 10;
+
+function enemyStatusBar(enemyHealth, enemyArmor, enemyFf) {
+    var total = enemyHealth + enemyArmor + enemyFf;
+    var healthPercent = (enemyHealth / total) * 100;
+    var armorPercent = (enemyArmor / total) * 100;
+    var ffPercent = (enemyFf / total) * 100;
+
+    healthEnemy.style.width = healthPercent + "%";
+    armorEnemy.style.width = armorPercent + "%";
+    ffEnemy.style.width = ffPercent + "%";
+}
+
+enemyStatusBar(enemyHealth, enemyArmor, enemyFf);
+
